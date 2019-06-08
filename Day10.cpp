de3dee64d8f20f2bd981d8bcf03a76c023872bc3
rahuljha12527 <45179877+rahuljha12527@users.noreply.github.com>
2019-06-08 09:48:16 +0530

    Update Day10.cpp

--- Day10.cpp
+++ Day10.cpp
@@ -4,25 +4,28 @@
 using namespace std;
 
 int main(){
-	int n;
+    int n;
     int rem=0,s=0,t=0;
-	
-	cin>>n;
-	while(n>0)
-	{
-		rem=n%2;
-		n=n/2;
-		if(rem==1)
-		{	s++;
-			if(s>=t)
+    
+    cin>>n;
+    while(n>0)
+    {
+        rem=n%2;
+        n=n/2;
+        if(rem==1)
+        {    s++;
+            if(s>=t)
 
-				t=s;
+                t=s;
 
-	}
-	else{
-		s=0;
-	}
+    }
+    else{
+        s=0;
+    }
 }
 
 
-cout<<t;
+cout<<t<<endl;
+
+return 0;
+}
